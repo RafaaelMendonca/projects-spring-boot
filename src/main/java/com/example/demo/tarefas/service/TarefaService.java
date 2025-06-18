8f2b5c1d28086b51f61759ac30e4dcdd021fd833
RafaaelMendonca <seu-email@dominio.com>
2025-06-18 14:40:22 -0300

    Adicionando o TarefaService para lidar com a regra de negócio do projeto

---
+++ src/main/java/com/example/demo/tarefas/service/TarefaService.java
@@ -0,0 +1,56 @@
+package com.example.demo.tarefas.service;
+
+import com.example.demo.tarefas.model.Tarefas;
+import com.example.demo.tarefas.repository.TarefaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TarefaService {
+    private final TarefaRepository tarefaRepository;
+
+    public TarefaService(TarefaRepository tarefaRepository){
+        this.tarefaRepository = tarefaRepository;
+    }
+
+    public void salvarTarefa(Tarefas tarefas){
+        if(tarefas.getDataTarefa().isBefore(LocalDate.now())){
+            throw new IllegalArgumentException("A data da tarefa não pode ser no passado");
+        }
+        tarefaRepository.salvarTarefa(tarefas);
+    }
+
+    public List<Tarefas> listarTarefas(){
+        return tarefaRepository.listarTarefas();
+    }
+
+    public void removerTarefas(String nomeTarefa){
+        if(nomeTarefa.trim().isEmpty()){
+            throw new IllegalArgumentException("Atente-se a preencher de forma correta o argumento");
+        }
+        tarefaRepository.removerTarefas(nomeTarefa);
+    }
+
+    public void editarTarefas(String nomeTarefa, Tarefas novaTarefa){
+        List<Tarefas> tarefa = tarefaRepository.listarTarefas();
+        boolean verificacao = false;
+        for(Tarefas t: tarefa){
+            if(t.getNomeTarefa().equalsIgnoreCase(nomeTarefa)){
+                verificacao = true;
+                break;
+            }
+        }
+        if(!verificacao){
+            throw new IllegalArgumentException("Nome da tarefa não encontrada");
+        }
+        tarefaRepository.editarTarefas(nomeTarefa, novaTarefa);
+    }
+
+    public Tarefas buscarPorIndice(int index){
+        if(tarefaRepository.buscarPorIndice(index) == null) {
+            throw new IllegalArgumentException("Índice não encontrado");
+        }
+        return tarefaRepository.buscarPorIndice(index);
+    }
+
+}
